c6c41040c9042b3c155a102f1b75212a92e4256d
ACB-prgm <63984796+ACB-prgm@users.noreply.github.com>
2021-06-14 10:38:33 -0400

    State Machine

---
+++ QA_Session_06_June14_21/StateMachine/Player.gd
@@ -0,0 +1,50 @@
+extends KinematicBody2D
+
+
+const MAX_VEL = 1000
+const ACCELERATION = 100
+
+var input_vector = Vector2.ZERO
+var velocity = Vector2.ZERO
+
+enum {
+	IDLE,
+	MOVE
+}
+var state = MOVE
+
+
+func _physics_process(_delta):
+	match state:
+		IDLE:
+			idle()
+		MOVE:
+			movement()
+	
+	print(state)
+
+
+func movement():
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * ACCELERATION
+		velocity = velocity.clamped(MAX_VEL)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, ACCELERATION)
+		
+		if velocity == Vector2.ZERO:
+			state = IDLE
+
+	move_and_slide(velocity)
+
+
+func idle():
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		state = MOVE
