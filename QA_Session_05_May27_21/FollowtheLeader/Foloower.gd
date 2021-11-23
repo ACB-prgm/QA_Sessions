8d3a2c01760fd61e90845a7698e158e02ad36bc7
ACB-prgm <63984796+ACB-prgm@users.noreply.github.com>
2021-11-23 12:09:51 -0500

    FollowLeader

--- QA_Session_05_May27_21/FollowtheLeader/Foloower.gd
+++ QA_Session_05_May27_21/FollowtheLeader/Foloower.gd
@@ -16,6 +16,12 @@ func _physics_process(delta):
 
 
 func follow():
-	if leader.points and leader.points.size() == leader.MAX_POINTS:
+	var points = leader.curve.get_baked_points()
+	if points:
+		var point = points[0]
 		if global_position.distance_to(leader.global_position) > 200:
-			global_position = leader.points.pop_front()
+			print("moving")
+			global_position = point
+			points.remove(0)
+#		elif global_position.distance_to(leader.global_position) <= 200:
+#			leader.points.clear()
--- QA_Session_05_May27_21/FollowtheLeader/Leader.gd
+++ QA_Session_05_May27_21/FollowtheLeader/Leader.gd
@@ -3,20 +3,24 @@ extends KinematicBody2D
 
 const MAX_VEL = 1000
 const ACCELERATION = 100
-const MAX_POINTS = 40
+const MAX_curve = 60
 
 var input_vector = Vector2.ZERO
 var velocity = Vector2.ZERO
-var points := []
+onready var start_pos = global_position
+onready var curve = $Path2D.curve
+onready var line = $Line2D
 
 
 func _ready():
+	
 	Globals.player = self
 
 
 func _physics_process(_delta):
+	line.global_position = start_pos
+	line.points = curve.get_baked_points()
 	movement()
-	create_points()
 
 
 func movement():
@@ -27,14 +31,16 @@ func movement():
 	if input_vector != Vector2.ZERO:
 		velocity += input_vector * ACCELERATION
 		velocity = velocity.clamped(MAX_VEL)
+		create_curve()
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, ACCELERATION)
 
 # warning-ignore:return_value_discarded
 	move_and_slide(velocity)
 
-func create_points():	
-	points.append(global_position)
+func create_curve():
+#	prints(curve.get_baked_points().size(), curve.get_point_count())
+	curve.add_point(global_position)
 	
-	if points.size() > MAX_POINTS:
-		points.pop_front()
+	if curve.get_point_count() > MAX_curve:
+		curve.remove_point(0)
